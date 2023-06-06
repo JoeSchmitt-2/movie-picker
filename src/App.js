e950a5dc338759c8a7ee485772731d1a83598348
JoeSchmitt-2 <84737443+JoeSchmitt-2@users.noreply.github.com>
2023-06-06 12:36:21 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+//By: Joseph Schmitt
+//Email: schmitttyy@gmail or jcs20fh@fsu.edu
+//Description: This is a movie picker app written using React!
 import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
-function App() {
+function Movie() {
+
+  //this state is used to store the value of the input
+  const [query,setQuerys] = useState('');
+
+  //this state is used to store the data from the api
+  const [container, setContainer] = useState([]);
+
+  //this takes the value of the input once submit is clicked and stores it in finalQuery
+  const [finalQuery, setFinalQuery] = useState('');
+
+  //this calls fetchMe() when the state 'query->finalQuery', aka input, is changed
+  useEffect(() => {
+    fetchMe()
+  },[finalQuery])
+
+  //fetching data from the api
+  //back ticks are used to add the value of the state 'query' to the url
+  const fetchMe = () =>{
+  fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=+${query}`, {
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-host": "online-movie-database.p.rapidapi.com",
+    "x-rapidapi-key": "70eb97ac5bmshb04b05b2e5ddfd6p1861c2jsn323041c6c66c"
+  }
+})
+
+//adding .json() returns a promise
+.then(response => {
+  return response.json();
+})
+//data is all of the elements from the api being stored in the state 'container'
+.then(data => {
+  setContainer(data.d)
+})
+//error catching
+.catch(err => {
+  console.error(err);
+});
+}
+
+//e is every value we type in
+//setQuerys is the changer that updates the state
+const onChangeHandler = (e) => {
+  setQuerys(e.target.value);
+}
+
+//allows the submit button to work
+//setFinalQuery having endpoint as a parameter allows us to use the value of endpoint once the 
+//button is clicked and used as the value for finalQuery which is set by setFinalQuery
+const submitHandler = (e) => {
+  e.preventDefault();
+  setFinalQuery(query);
+}
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Movie Picker</h1>
+
+      <form onSubmit={ submitHandler }>
+        <input type="text" value={ query } onChange={ onChangeHandler } />
+        <button type="submit">Sumbit</button>
+      </form>
+  
+    <div className="element">
+      {container.map(( item, index ) => {
+        return (
+          <div key={index} className='element-div'> 
+            <img src={ item.i.imageUrl } alt="" />
+            <p>{ item.l }</p>
+            <p>Actors: { item.s }</p>
+            <p>{ item.qid }</p>
+            <p>{ item.y }</p>
+          </div>
+    
+        );
+      })}
+    </div>
     </div>
   );
 }
 
-export default App;
+export default Movie;
